4f6a52f105064f7e65b304ab03a8f26b5f0f043c
Justin Paul Silva <justin@jpsilva.com>
2024-04-22 13:28:04 -0400

    fix: asyncify type breaking prisma type inference

--- packages/core/src/types/IntegroClient.ts
+++ packages/core/src/types/IntegroClient.ts
@@ -1,6 +1,8 @@
-import { Asyncify } from 'type-fest';
+import { SetReturnType } from 'type-fest';
 import { LazyModule } from '../lazy';
 import { IntegroApp, Handler } from './IntegroApp';
+
+type Asyncify<Fn extends (...arguments_: any[]) => any> = ReturnType<Fn> extends Promise<unknown> ? Fn : SetReturnType<Fn, Promise<ReturnType<Fn>>>;
 
 export type IntegroClient<T extends IntegroApp> =
   T extends LazyModule<infer U>
